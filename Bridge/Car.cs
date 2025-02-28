c4fb6c3b4ec5037ff3709045f3f5ac7e40588834
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-02-28 11:18:08 +0100

    Implemented class, properties, and method. Also documentation.

---
+++ Bridge/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace Bridge;
+
+/// <summary>
+/// A Car class for storing information about a car, 
+/// getting the price for crossing the bridge, and 
+/// what kind of vehicle it is.
+/// </summary>
+public class Car
+{
+    /// <value>
+    /// The <c>LicensePlate</c> property represents a 
+    /// car's license plate for this instance.
+    /// </value>
+    public string LicensePlate { get; set; }
+
+    /// <value>
+    /// The <c>Date</c> property represents a 
+    /// car's allowed crossing date for this instance.
+    /// </value>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Gets the car's crossing price and returns it.
+    /// </summary>
+    public double Price() => 230;
+
+    /// <summary>
+    /// Gets the car's vehicle type and returns it.
+    /// </summary>
+    public string VehicleType() => "Car";
+}
