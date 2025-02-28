0f6642593be04d13d17e0f424d56604c4966d86d
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-02-28 11:18:25 +0100

    Created unit tests for Car class. All tests passed.

---
+++ BridgeTest/CarTests.cs
@@ -0,0 +1,27 @@
+﻿using Bridge;
+
+namespace BridgeTest;
+
+[TestClass]
+public sealed class CarTests
+{
+    [TestMethod]
+    public void FixedCarPrice()
+    {
+        var car = new Car();
+
+        double carPrice = car.Price();
+
+        Assert.IsTrue(carPrice == 230);
+    }
+
+    [TestMethod]
+    public void VehicleTypeIsCar()
+    {
+        var car = new Car();
+
+        string carType = car.VehicleType();
+
+        Assert.IsTrue(carType == "Car");
+    }
+}
