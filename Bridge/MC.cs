b628c76a3699c60abceb2443eb8d6c81067b988a
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-02-28 11:30:54 +0100

    The class is now contained within its own file.

--- Bridge/MC.cs
+++ Bridge/MC.cs
@@ -1,31 +1,31 @@
 ﻿namespace Bridge;
 
 /// <summary>
-/// A Car class for storing information about a car, 
+/// An MC class for storing information about a motorcycle, 
 /// getting the price for crossing the bridge, and 
 /// what kind of vehicle it is.
 /// </summary>
-public class Car
+public class MC
 {
     /// <value>
     /// The <c>LicensePlate</c> property represents a 
-    /// car's license plate for this instance.
+    /// motorcycle's license plate for this instance.
     /// </value>
     public string LicensePlate { get; set; }
 
     /// <value>
     /// The <c>Date</c> property represents a 
-    /// car's allowed crossing date for this instance.
+    /// motorcycle's allowed crossing date for this instance.
     /// </value>
     public DateTime Date { get; set; }
 
     /// <summary>
-    /// Gets the car's crossing price and returns it.
+    /// Gets the motorcycle's crossing price and returns it.
     /// </summary>
-    public double Price() => 230;
+    public double Price() => 120;
 
     /// <summary>
-    /// Gets the car's vehicle type and returns it.
+    /// Gets the motorcycle's vehicle type and returns it.
     /// </summary>
-    public string VehicleType() => "Car";
+    public string VehicleType() => "MC";
 }
