55bad6cb0bf95ff247543b493e900f7c8a90d14c
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-02-28 11:29:20 +0100

    Implemented MC class with methods along with unit test for it. All tests passed.

--- Bridge/Car.cs
+++ Bridge/Car.cs
@@ -29,3 +29,33 @@ public class Car
     /// </summary>
     public string VehicleType() => "Car";
 }
+
+/// <summary>
+/// An MC class for storing information about a motorcycle, 
+/// getting the price for crossing the bridge, and 
+/// what kind of vehicle it is.
+/// </summary>
+public class MC
+{
+    /// <value>
+    /// The <c>LicensePlate</c> property represents a 
+    /// motorcycle's license plate for this instance.
+    /// </value>
+    public string LicensePlate { get; set; }
+
+    /// <value>
+    /// The <c>Date</c> property represents a 
+    /// motorcycle's allowed crossing date for this instance.
+    /// </value>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Gets the motorcycle's crossing price and returns it.
+    /// </summary>
+    public double Price() => 120;
+
+    /// <summary>
+    /// Gets the motorcycle's vehicle type and returns it.
+    /// </summary>
+    public string VehicleType() => "MC";
+}
---
+++ BridgeTest/MCTests.cs
@@ -0,0 +1,27 @@
+﻿using Bridge;
+
+namespace BridgeTest;
+
+[TestClass]
+public sealed class MCTests
+{
+    [TestMethod]
+    public void FixedMCPrice()
+    {
+        var mc = new MC();
+
+        double mcPrice = mc.Price();
+
+        Assert.IsTrue(mcPrice == 120);
+    }
+
+    [TestMethod]
+    public void VehicleTypeIsMC()
+    {
+        var mc = new MC();
+
+        string mcType = mc.VehicleType();
+
+        Assert.IsTrue(mcType == "MC");
+    }
+}
