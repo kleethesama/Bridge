65dc02eef8abd45ea5b0789fa457527463431eb5
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-03-02 09:56:04 +0100

    Written test for discount (Brobizz) for vehicles. Test passed.

---
+++ BridgeTest/PriceTests.cs
@@ -0,0 +1,21 @@
+using Bridge;
+using Bridge.Price_classes;
+
+namespace BridgeTest;
+
+[TestClass]
+public class PriceTests
+{
+    [TestMethod]
+    public void ApplyBrobizzDiscountToCar()
+    {
+        var vehicle = new Car("HF38663", DateTime.Now);
+        var brobizz = new Brobizz(vehicle);
+
+        double originalPrice = brobizz.Vehicle.Price();
+        double newPrice = brobizz.Vehicle.Price(brobizz); // Price overload
+
+        // Doing a floating-point operation to check for floating-point error.
+        Assert.AreEqual(newPrice, originalPrice * (1.0 - 0.1));
+    }
+}
