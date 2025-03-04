efc69629e3849fa6cfe1eda35320163754b750e2
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-03-04 10:04:57 +0100

    Finished documentation for the BrobizzWeekend class.

--- StoreBaeltTicketLibrary/BrobizzWeekend.cs
+++ StoreBaeltTicketLibrary/BrobizzWeekend.cs
@@ -4,8 +4,19 @@ using StoreBaeltTicketLibrary.Exception_classes;
 
 namespace StoreBaeltTicketLibrary;
 
+/// <summary>
+/// A class for the Brobizz weekend price discount. 
+/// Applies only to cars
+/// </summary>
 public class BrobizzWeekend : Brobizz
 {
+    /// <summary>
+    /// Initializes an instance of a <c>BrobizzWeekend</c>
+    /// with a <c>Vehicle</c> property.
+    /// </summary>
+    /// <param name="vehicle"></param>
+    /// <exception cref="VehicleIsNotCarException"></exception>
+    /// <exception cref="DateIsNotWeekendException"></exception>
     public BrobizzWeekend(Vehicle vehicle) : base(vehicle)
     {
         if (Vehicle.GetType() != typeof(Car))
@@ -18,6 +29,14 @@ public class BrobizzWeekend : Brobizz
         }
     }
 
+    /// <summary>
+    /// A method used to check if this instance's date is a weekend day or not.
+    /// </summary>
+    /// <returns>
+    /// <see langword="true"/> if the <c>Vehicle</c> property's 
+    /// <c>Date</c> property is a Saturday or Sunday, 
+    /// otherwise <see langword="false"/>.
+    /// </returns>
     private bool CheckIfDateIsWeekend()
     {
         return Vehicle.Date.DayOfWeek == DayOfWeek.Saturday
