9385c33bcabf4029c7a64ed5b043de9ed44145ff
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-03-04 09:25:06 +0100

    Added documentation to exception classes.

--- StoreBaeltTicketLibrary/Exception classes/DateIsNotWeekendException.cs
+++ StoreBaeltTicketLibrary/Exception classes/DateIsNotWeekendException.cs
@@ -1,7 +1,12 @@
 ﻿namespace StoreBaeltTicketLibrary.Exception_classes;
 
+/// <summary>
+/// The exception that is thrown when a <c>DateTime</c> 
+/// object is not of week day Saturday or Sunday.
+/// </summary>
 public class DateIsNotWeekendException : Exception
 {
+    #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public DateIsNotWeekendException() : base() { }
 
     public DateIsNotWeekendException(string message) : base(message) { }
--- StoreBaeltTicketLibrary/Exception classes/VehicleIsNotCarException.cs
+++ StoreBaeltTicketLibrary/Exception classes/VehicleIsNotCarException.cs
@@ -1,7 +1,12 @@
 ﻿namespace StoreBaeltTicketLibrary.Exception_classes;
 
+/// <summary>
+/// The exception that is thrown when a <c>Vehicle</c> 
+/// object is not a <c>Car</c> object.
+/// </summary>
 public class VehicleIsNotCarException : Exception
 {
+    #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public VehicleIsNotCarException() : base() { }
 
     public VehicleIsNotCarException(string message) : base(message) { }
