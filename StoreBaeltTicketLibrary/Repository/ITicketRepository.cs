2684faec7b48b28ae18502f7cd05356a88e7066f
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-03-06 16:22:36 +0100

    Implemented repository. Next up are tests.

---
+++ StoreBaeltTicketLibrary/Repository/ITicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bridge;
+
+namespace StoreBaeltTicketLibrary.Repository;
+
+public interface ITicketRepository
+{
+    public void Add(Vehicle vehicle);
+    public IEnumerable<Vehicle> Get(string licensePlate);
+    public IEnumerable<Vehicle> GetAll();
+}
---
+++ StoreBaeltTicketLibrary/Repository/TicketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Bridge;
+
+namespace StoreBaeltTicketLibrary.Repository;
+
+public class TicketRepository : ITicketRepository
+{
+    public static List<Vehicle> Vehicles;
+
+    public TicketRepository()
+    {
+        Vehicles = [];
+    }
+
+    public TicketRepository(List<Vehicle> vehicles)
+    {
+        Vehicles = vehicles;
+    }
+
+    public void Add(Vehicle vehicle)
+    {
+        Vehicles.Add(vehicle);
+    }
+
+    public IEnumerable<Vehicle>? Get(string licensePlate)
+    {
+        var vehicleSearch = Vehicles.FindAll(e => e.LicensePlate == licensePlate);
+        if (vehicleSearch.Count > 0)
+        {
+            return vehicleSearch;
+        }
+        return null;
+    }
+
+    public IEnumerable<Vehicle>? GetAll()
+    {
+        if (Vehicles.Count > 0)
+        {
+            return Vehicles;
+        }
+        return null;
+    }
+}
